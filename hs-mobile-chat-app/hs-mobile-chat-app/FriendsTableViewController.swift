fc5fab8ec34e670834f8a19e497fb3489d4b2c94
Matheus Ruschel <matheus.ruschel@gmail.com>
2016-05-22 15:16:19 -0300

    fixed layout issues

--- hs-mobile-chat-app/hs-mobile-chat-app/FriendsTableViewController.swift
+++ hs-mobile-chat-app/hs-mobile-chat-app/FriendsTableViewController.swift
@@ -21,10 +21,9 @@ class FriendsTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
 //         self.navigationItem.rightBarButtonItem = self.editButtonItem()
 		friends = NSMutableArray()
-		friends.addObject("Friend1")
-		friends.addObject("Friends2")
+		friends.addObject("General chat")
 		
-		navigationItem.title = "FireChat"
+		navigationItem.title = "HootChat"
 		let nibCell = UINib(nibName: "FriendTableViewCell", bundle: nil)
 		tableView.registerNib(nibCell, forCellReuseIdentifier: "FriendCell")
     }
@@ -67,48 +66,5 @@ class FriendsTableViewController: UITableViewController {
 		self.showViewController(chatViewController, sender: self)
 	}
 
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
 
 }
